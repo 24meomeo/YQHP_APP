2df23ea2a7368e7b925e2e0e7ed9a1e3dd6f36e5
ThHine <generaltaikhoan@gmail.com>
2023-10-21 11:41:37 +0700

    themQuanLyChinh

---
+++ app/src/main/java/com/example/yqhd_app/QuanLy/QuanLyActivity.java
@@ -0,0 +1,16 @@
+package com.example.yqhd_app.QuanLy;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yqhd_app.R;
+
+public class QuanLyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quanly_activity_quanly);
+    }
+}
