4afbec3af139c5c4f06e829420e4271ccd410eb4
ThHine <generaltaikhoan@gmail.com>
2023-10-28 14:33:56 +0700

    updateUIQuanLyChinh

--- app/src/main/java/com/example/yqhd_app/QuanLy/Fragment/khohangFragment.java
+++ app/src/main/java/com/example/yqhd_app/QuanLy/Fragment/khohangFragment.java
@@ -6,12 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import com.example.yqhd_app.QuanLy.TabFragment.customViewPager;
+import com.example.yqhd_app.QuanLy.TabFragment.khohangViewPagerAdapter;
 import com.example.yqhd_app.R;
-
+import com.google.android.material.tabs.TabLayout;
 
 public class khohangFragment extends Fragment {
-
+    private TabLayout tabLayout;
+    private customViewPager viewPager;
+    private View v;
     public khohangFragment() {
         // Required empty public constructor
     }
@@ -26,6 +31,15 @@ public class khohangFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quanly_khohang, container, false);
+        v = inflater.inflate(R.layout.fragment_quanly_khohang, container, false);
+        tabLayout = v.findViewById(R.id.tablayoutkhohang);
+        viewPager = v.findViewById(R.id.viewpagerkhohang);
+
+        khohangViewPagerAdapter adapter = new khohangViewPagerAdapter(getChildFragmentManager()
+        , FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+        viewPager.setPagingEnabled(false);
+        tabLayout.setupWithViewPager(viewPager);
+        return v;
     }
 }
---
+++ app/src/main/java/com/example/yqhd_app/QuanLy/TabFragment/khohangViewPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.yqhd_app.QuanLy.TabFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class khohangViewPagerAdapter extends FragmentStatePagerAdapter {
+    public khohangViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new SanPhamFragment();
+            case 1:
+                return new ThuongHieuFragment();
+            default:
+                return new SanPhamFragment();
+//            case 2:
+//                return new taikhoanFragment();
+        }
+//        return null;
+    }
+    //Trả về số lượng item (tương ứng với từng trang ở thanh trượt dưới)
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+//        return super.getPageTitle(position);
+        switch (position){
+            case 0:
+                return "Tab 1";
+            case 1:
+                return "Tab 2";
+            default:
+                return "Tab 1";
+        }
+    }
+}
