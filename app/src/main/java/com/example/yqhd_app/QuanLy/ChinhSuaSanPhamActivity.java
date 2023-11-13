fefb2ee1058e18c7bbb70eeb1a83f51f4a7cdb49
24meomeo <meomeo2483@gmail.com>
2023-11-13 22:24:33 +0700

    themSuaSPActivity&UI

---
+++ app/src/main/java/com/example/yqhd_app/QuanLy/ChinhSuaSanPhamActivity.java
@@ -0,0 +1,16 @@
+package com.example.yqhd_app.QuanLy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yqhd_app.R;
+
+public class ChinhSuaSanPhamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chinh_sua_san_pham);
+    }
+}
