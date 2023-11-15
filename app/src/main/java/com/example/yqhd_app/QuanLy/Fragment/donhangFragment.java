9f53241e92c4b8dae1a8f2d216b2916c14ea0a8a
24meomeo <meomeo2483@gmail.com>
2023-11-15 11:18:00 +0700

    updateDonHangFragment&Adapter

--- app/src/main/java/com/example/yqhd_app/QuanLy/Fragment/donhangFragment.java
+++ app/src/main/java/com/example/yqhd_app/QuanLy/Fragment/donhangFragment.java
@@ -6,11 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import com.example.yqhd_app.QuanLy.QuanLyActivity;
+import com.example.yqhd_app.QuanLy.TabFragment.customViewPager;
+import com.example.yqhd_app.QuanLy.TabFragment.donhangViewPagerAdapter;
+import com.example.yqhd_app.QuanLy.TabFragment.khohangViewPagerAdapter;
 import com.example.yqhd_app.R;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class donhangFragment extends Fragment {
+    private TabLayout tabLayout;
+    private customViewPager viewPager;
+    private View v;
     public donhangFragment() {
         // Required empty public constructor
     }
@@ -25,6 +34,15 @@ public class donhangFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quanly_donhang, container, false);
+        v = inflater.inflate(R.layout.fragment_quanly_donhang, container, false);
+        tabLayout = v.findViewById(R.id.tablayoutdonhang);
+        viewPager = v.findViewById(R.id.viewpagerdonhang);
+
+        donhangViewPagerAdapter adapter = new donhangViewPagerAdapter(getChildFragmentManager()
+                , FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+        viewPager.setPagingEnabled(false);
+        tabLayout.setupWithViewPager(viewPager);
+        return v;
     }
 }
