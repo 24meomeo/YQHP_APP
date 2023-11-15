8fee35dba1a8dbef95a231c594e2746d90b0e3c3
24meomeo <meomeo2483@gmail.com>
2023-11-16 02:32:16 +0700

    themQLTaiKhoanAdapter

--- app/src/main/java/com/example/yqhd_app/QuanLy/Fragment/taikhoanFragment.java
+++ app/src/main/java/com/example/yqhd_app/QuanLy/Fragment/taikhoanFragment.java
@@ -6,12 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import com.example.yqhd_app.QuanLy.TabFragment.customViewPager;
+import com.example.yqhd_app.QuanLy.TabFragment.donhangViewPagerAdapter;
+import com.example.yqhd_app.QuanLy.TabFragment.taikhoanViewPagerAdapter;
 import com.example.yqhd_app.R;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class taikhoanFragment extends Fragment {
-
+    private TabLayout tabLayout;
+    private customViewPager viewPager;
+    private View v;
     public taikhoanFragment() {
         // Required empty public constructor
     }
@@ -25,7 +32,15 @@ public class taikhoanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quanly_taikhoan, container, false);
+        v = inflater.inflate(R.layout.fragment_quanly_taikhoan, container, false);
+        tabLayout = v.findViewById(R.id.tablayouttaikhoan);
+        viewPager = v.findViewById(R.id.viewpagertaikhoan);
+
+        taikhoanViewPagerAdapter adapter = new taikhoanViewPagerAdapter(getChildFragmentManager()
+                , FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+        viewPager.setPagingEnabled(false);
+        tabLayout.setupWithViewPager(viewPager);
+        return v;
     }
 }
---
+++ app/src/main/java/com/example/yqhd_app/QuanLy/TabFragment/taikhoanViewPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.yqhd_app.QuanLy.TabFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class taikhoanViewPagerAdapter extends FragmentStatePagerAdapter {
+    public taikhoanViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new chuaXacNhanFragment();
+            case 1:
+                return new daXacNhanFragment();
+            default:
+                return new chuaXacNhanFragment();
+//            case 2:
+//                return new taikhoanFragment();
+        }
+//        return null;
+    }
+    //Trả về số lượng item (tương ứng với từng trang ở thanh trượt dưới)
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+//        return super.getPageTitle(position);
+        switch (position){
+            case 0:
+                return "Khách Hàng";
+            case 1:
+                return "Nhân Viên";
+            default:
+                return "Khách Hàng";
+        }
+    }
+}
